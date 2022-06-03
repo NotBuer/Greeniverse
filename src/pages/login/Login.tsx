ebe89bd4e072e58930cdc09ce561b806599c5561
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-06-03 15:14:57 -0300

    @MuriloAGama [Task: Adding new features to the login page] {03/06/2022 15:15}

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate, } from 'react-router-dom';
 import GoogleIcon from '@mui/icons-material/Google';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import ArrowBackSharpIcon from '@mui/icons-material/ArrowBackSharp';
 import './Login.css';
 
 function Login() {
@@ -13,14 +14,40 @@ function Login() {
                 <section>
                     <article id='form_login'>
 
-
-                        <Typography variant='h3' gutterBottom component='h3'> Entrar </Typography>
+                            <a id='Iconback' href="http://localhost:3000/home" target="black" rel="noopener noreferrer">
+                                <ArrowBackSharpIcon style={{ fontSize: 30, color: "black", alignItems: "flex-start" }} />
+                            </a>
+                       
+                        <Typography variant='h3' gutterBottom component='h3' className='text2'> Entrar </Typography>
 
                         <TextField id='user' label='E-mail' variant='outlined' name='user' margin='normal' size='small' />
                         <TextField id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password' size='small' />
 
 
-                        <Button className='buttonLogin' type='submit' variant='contained' size="small"> Login </Button>
+                        <Button className='buttonLogin' type='submit' variant='contained' size="small" > Login </Button>
+
+                        <Box borderBottom={1}>
+                            <Link to='/registeruser'>
+                                <Typography variant='subtitle1' gutterBottom align='center' className='texts1'> Não tem uma conta ainda? Cadastre-se </Typography>
+                            </Link>
+
+                            <p className='ForgotThePass'>Ou</p>
+                        </Box>
+
+                        <Box className='Icons' display='flex' justifyContent='center' marginTop={3}>
+
+                            <a href="" target="black" rel="noopener noreferrer">
+                                <GoogleIcon style={{ fontSize: 40, color: "red" }} />
+                            </a>
+
+                            <a href="" target="black" rel="noopener noreferrer">
+                                <FacebookIcon style={{ fontSize: 40, color: "blue" }} />
+                            </a>
+
+                            <a href="" target="black" rel="noopener noreferrer">
+                                <TwitterIcon style={{ fontSize: 40, color: "blue" }} />
+                            </a>
+                        </Box>
                     </article>
                     <article id='img'>
                         <img src="https://i.imgur.com/G5Vhf5L.png" alt="Logo" />

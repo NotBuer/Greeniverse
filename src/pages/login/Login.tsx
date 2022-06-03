57552b1229f05ad6fd6eceb0ee6cda040eef3507
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-06-02 22:19:06 -0300

    @MuriloAGama [Task: Implementing Page of Login] {02/06/2022 22:20}

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,36 +2,31 @@ import React, { useState, ChangeEvent } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
 import { Link, useNavigate, } from 'react-router-dom';
 import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import './Login.css';
 
-function Login(){
-    return(
+function Login() {
+    return (
         <>
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid alignItems='center' xs={6}>
-                <Box paddingX={20}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='texts1'> Enter </Typography>
-                        <TextField id='user' label='User' variant='outlined' name='user' margin='normal' fullWidth />
-                        <TextField id='password' label='Password' variant='outlined' name='password' margin='normal' type='password' fullWidth />
-                        <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary'> Login </Button>
-                        </Box>
-                    </form>
-                    <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'> Don't have an account yet? </Typography>
-                        </Box>
-                        <Link to='/registeruser'>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='texts1'> Sign-up </Typography>
-                        </Link>
-                    </Box>
-                </Box>
-            </Grid>
-            <Grid xs={6} className='image'>
+            <main>
+                <section>
+                    <article id='form_login'>
 
-            </Grid>
-        </Grid>
+
+                        <Typography variant='h3' gutterBottom component='h3'> Entrar </Typography>
+
+                        <TextField id='user' label='E-mail' variant='outlined' name='user' margin='normal' size='small' />
+                        <TextField id='password' label='Senha' variant='outlined' name='password' margin='normal' type='password' size='small' />
+
+
+                        <Button className='buttonLogin' type='submit' variant='contained' size="small"> Login </Button>
+                    </article>
+                    <article id='img'>
+                        <img src="https://i.imgur.com/G5Vhf5L.png" alt="Logo" />
+                    </article>
+                </section>
+            </main>
         </>
     );
 }

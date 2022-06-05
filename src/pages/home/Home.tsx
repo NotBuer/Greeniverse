559e97d53c4ac3f2f70a5e2fa7fb8f92a7c7e28a
cateldev <catelmatt@hotmail.com>
2022-06-05 00:08:57 -0300

    @cateldev [Task: Scrollbar Style] {05/06/2022}

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../../components/statics/navbar/Navbar';
+import Footer from '../../components/statics/footer/Footer';
 
 function Home(){
     return(
         <>
         <Navbar />
+
+        <Footer />
         </>
     );
 }

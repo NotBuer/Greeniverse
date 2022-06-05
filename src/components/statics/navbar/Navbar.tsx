dfff2ff86f1e92d454d583ca7595717f12b6ff67
cateldev <catelmatt@hotmail.com>
2022-06-04 21:31:25 -0300

    @cateldev [Task: Add Img] {04/06/2022}

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -3,42 +3,44 @@ import './Navbar.css'
 
 function Navbar() {
     return (
-        <nav>
+      <nav>
+
         <div className="top-promo-banner">
             <span>Frete grátis nas compras acima de R$200,00</span>
         </div>
 
-  <header className="top-nav-bar">
+        <header className="top-nav-bar">
 
-    <img className="logo" src="./src/img/logo.png" alt="logo"/>
-    <form className="search-box">
-      <img className="lupa-icon" src="./src/img/lupa-icon.png" alt="lupa"/>
-      <input type="text" placeholder="O que voce está procurando?"/>
-      <button type="submit">Buscar</button>
-    </form>
+          <img className="logo" src="./src/img/logo.png" alt="logo"/>
 
-    <a href="/">Minha conta
-      <img className="nav-icon-1" src="./src/img/profile.png" alt="cart"/>
-    </a>
+          <form className="search-box">
+            <img className="lupa-icon" src="./src/img/lupa-icon.png" alt="lupa"/>
+            <input type="text" placeholder="O que voce está procurando?"/>
+            <button type="submit">Buscar</button>
+          </form>
 
-    <a href="/"> Minha sacola
-      <img className="nav-icon-2" src="./src/img/shopping-bag.png" alt="user"/>
-    </a>
+          <a href="/">Minha conta
+            <img className="nav-icon-1" src="./src/img/profile.png" alt="cart"/>
+          </a>
 
-  </header>
-  <div className="botton-menu-bar">
-    <ul className="botton-list">
-      <li><a href="/">Ofertas</a></li>
-      <li><a href="/">Hortifruti</a></li>
-      <li><a href="/">Mercearia</a></li>
-      <li><a href="/">Laticínios</a></li>
-      <li><a href="/">Ovos e Carnes</a></li>
-    </ul>
-  </div>
+          <a href="/"> Minha sacola
+            <img className="nav-icon-2" src="./src/img/shopping-bag.png" alt="user"/>
+          </a>
 
-  </nav>
+        </header>
 
-    )
+      <div className="botton-menu-bar">
+        <ul className="botton-list">
+          <li><a href="/">Ofertas</a></li>
+          <li><a href="/">Hortifruti</a></li>
+          <li><a href="/">Mercearia</a></li>
+          <li><a href="/">Laticínios</a></li>
+          <li><a href="/">Ovos e Carnes</a></li>
+        </ul>
+      </div>
+
+    </nav>
+  )
 }
 
 export default Navbar;

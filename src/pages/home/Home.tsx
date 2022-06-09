d620adda9bb10d956d7eebfa1f68c72ae44a8707
cateldev <catelmatt@hotmail.com>
2022-06-09 15:30:36 -0300

    @cateldev [Task:Features Implementation]

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import Navbar from '../../components/statics/navbar/Navbar';
 import Banner from '../../components/statics/banner/Banner';
+import Features from '../../components/statics/features/Features';
 
 import Footer from '../../components/statics/footer/Footer';
 
@@ -10,6 +11,7 @@ function Home(){
         <>
         <Navbar />
         <Banner />
+        <Features />
 
 
         </>

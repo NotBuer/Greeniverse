25673dc1b4c7951faa90191a84f9feb2b570a778
kaufariax <eikauanefarias@gmail.com>
2022-06-09 18:35:14 -0300

    @kaufariax [Task: Pages Shopping Cart]{09/06/2022 18:35}

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import ShoppingCart from './pages/shoppingCart/ShoppingCart';
 import RegisterUser from './pages/register/RegisterUser';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/home" element={<Home />} />
-
+                    <Route path='/cart' element={<ShoppingCart />} />
                 </Routes>
 
         </Router>

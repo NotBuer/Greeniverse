71af502b9401215d656b129eeb85eea327c1c541
kaufariax <eikauanefarias@gmail.com>
2022-06-13 10:26:28 -0300

    @kaufariax [Task: Products Card]{13/06/2022 10:27}

---
+++ src/models/StockDTO.ts
@@ -0,0 +1,10 @@
+interface Stock {
+    productCategory?: string,
+    description?: string,
+    price?: number,
+    productName?: string,
+    provider?: string,
+    productPhoto?: string
+
+}
+export default Stock;

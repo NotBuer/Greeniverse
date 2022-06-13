91815d274ccaec2d1f3dfac822a4c1179b6d1c3d
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-06-13 14:11:40 -0300

    @GabrielaPeres [Task:correction heads ] {13/06/2022- 14:12}

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,39 @@
 import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/statics/navbar/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import ShoppingCart from './pages/shoppingCart/ShoppingCart';
 import RegisterUser from './pages/register/RegisterUser';
 import './App.css';
 import store from './store/store';
-<<<<<<< HEAD
 import Providers from './pages/providers/Providers'
-=======
 import AboutUs from './pages/aboutUs/AboutUs';
 import Product from './components/statics/product/Product';
-import Providers from './pages/providers/Providers';
-    
 
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
+
+
 function App() {
     useEffect(() => {
         document.title = "Greeniverse";
     }, []);
 
-<<<<<<< HEAD
-    return ( 
-             <Provider store={store}>
-                <Router>
-                    <Routes>
-=======
     return (
 
-  
         <Provider store={store}>
             <Router>
                 <Routes>
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/home" element={<Home />} />
                     <Route path='/cart' element={<ShoppingCart />} />
                     <Route path='/registeruser' element={<RegisterUser />} />
-<<<<<<< HEAD
-                    <Route path='/providers' element={<Providers/>} />
-=======
+                    <Route path='/providers' element={<Providers />} />
                     <Route path='/aboutus' element={<AboutUs />} />
                     <Route path='/products' element={<Product />} />
-                    <Route path='/providers' element={<Providers />} />
-                </Routes>
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
-
                 </Routes>
             </Router>
-<<<<<<< HEAD
-        </Provider>       
-=======
         </Provider>
-
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
     )
 }
 
--- src/pages/providers/Providers.tsx
+++ src/pages/providers/Providers.tsx
@@ -1,75 +1,36 @@
-<<<<<<< HEAD
-import { Button,Grid,Typography,TextField} from "@material-ui/core";
+import { Button, Grid, Typography, TextField } from "@material-ui/core";
 import { Link } from 'react-router-dom';
 import { Box } from "@mui/material";
 import './Providers.css'
 import { ChangeEvent } from "react";
-=======
-import { Button,Grid,Typography,TextField } from "@material-ui/core";
-import { Link } from 'react-router-dom';
-import { Box } from "@mui/material";
 
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
-
-function Providers(){
-  return(
+function Providers() {
+  return (
     <>
-<<<<<<< HEAD
+      <Grid direction='column' justifyContent='flex-start' alignItems='flex-end'>
+        <Grid item xs={4} alignItems='center'>
+          <Typography variant='h5' gutterBottom component='h5' className='text'> Greeniverse </Typography>
+          <Grid direction='column' justifyContent='flex-start' alignItems='center'></Grid>
+          <Box>
+            <form >
+              <Typography variant='h4' gutterBottom component='h4' align='center' className='text3'> Enviar uma solicitação </Typography>
+              <TextField id='none' label='E-mail' variant='filled' name='email' margin='normal' fullWidth />
+              <TextField id='none' label='CNPJ' variant='filled' name='CNPJ' margin='normal' fullWidth />
+              <TextField id='none' label='Assunto' variant='filled' name='about' margin='normal' fullWidth />
+              <TextField id='none' label='Descrição' variant='filled' name='descrption' margin='normal' fullWidth />
 
-    <Grid direction='column' justifyContent='flex-start' alignItems='flex-end'>
-    <Grid item xs={4} alignItems='center'>
- 
-         <Box>
-     <form >
-     <Typography variant='h5' gutterBottom component='h5' className='text'> Greeniverse </Typography>
-=======
-    <Grid direction='column' justifyContent='flex-start' alignItems='center'></Grid>
-     <Box>
-     <form >
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
-     
-        <Typography variant='h4' gutterBottom component='h4' align='center' className='text3'> Enviar uma solicitação </Typography>
+              <Typography variant='h6' gutterBottom component='h6' align='center' className='text4'> Insira os detalhes de sua solicitação. Um membro de nossa equipe responderá assim que for possível. </Typography>
 
-          <TextField id='none' label='E-mail' variant='filled' name='email' margin='normal' fullWidth />
-          <TextField id='none' label='CNPJ' variant='filled'name='CNPJ' margin='normal' fullWidth />
-          <TextField  id='none' label='Assunto' variant='filled' name='about' margin='normal' fullWidth />
-<<<<<<< HEAD
-          <TextField id='none' label='Descrição' variant='filled'  name='descrption' margin='normal' fullWidth />
-
-          <Typography variant='h6' gutterBottom component='h6' align='center' className='text4'> Insira os detalhes de sua solicitação. Um membro de nossa equipe responderá assim que for possível. </Typography>
-       
-          <Box marginTop={5} textAlign='center'>                    
-          <Button  type='submit' variant='contained' className='buttonProviders'>Enviar </Button>                 
-                  
-           </Box>
-        </form> 
-            </Box>
-          </Grid>
-      </Grid>
-        </>
-  );
-}
-export default Providers;                                               
-       
-       
-=======
-          <TextField id='none' label='descrição' variant='filled'  name='descrption' margin='normal' fullWidth />
-
-          <Typography variant='h6' gutterBottom component='h6' align='center' className='text4'> Insira os detalhes de sua solicitação. Um membro de nossa equipe responderá assim que for possível. Ass:Equipe Greeniverse </Typography>
-
-          <Box marginTop={4} textAlign='center'>
-            <Button  type='submit' variant='contained' className='btn'>              
-              Enviar        
-            </Button> 
-            <Box marginY={2} textAlign='center' className='button1'></Box>        
-                                                          
-         <Link to='/home' className='text-decorator-none'></Link>  
-      </Box>
-    </form>
-         </Box>
-      
+              <Box marginTop={5} textAlign='center'>
+                <Button type='submit' variant='contained' className='buttonProviders'>Enviar </Button>
+                <Link to='/home' className='text-decorator-none'></Link>
+              </Box>
+            </form>
+          </Box>
+        </Grid>
+     </Grid>
     </>
   );
 }
 export default Providers;
->>>>>>> fd9093dfe26f0d6a829280ebae383d921b64a5c2
+

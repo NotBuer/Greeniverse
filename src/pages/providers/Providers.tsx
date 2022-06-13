d36698b3080fe1043682d775678d4aba6d685791
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-06-13 15:11:27 -0300

    @GabrielaPeres [Task:correction ] {13/06/2022- 15:12}

--- src/pages/providers/Providers.tsx
+++ src/pages/providers/Providers.tsx
@@ -8,15 +8,8 @@ import { ChangeEvent } from "react";
 function Providers() {
   return (
     <>
-<<<<<<< HEAD
     <Navbar />
-    <Grid direction='column' justifyContent='flex-start' alignItems='center'></Grid>
-     <Box>
-     <form >
-     
-        <Typography variant='h4' gutterBottom component='h4' align='center' className='text3'> Enviar uma solicitação </Typography>
-=======
-      <Grid direction='column' justifyContent='flex-start' alignItems='flex-end'>
+    <Grid className="gridBox"direction='column' justifyContent='flex-start' alignItems='flex-end'>
         <Grid item xs={4} alignItems='center'>
           <Typography variant='h5' gutterBottom component='h5' className='text'> Greeniverse </Typography>
           <Grid direction='column' justifyContent='flex-start' alignItems='center'></Grid>
@@ -27,7 +20,6 @@ function Providers() {
               <TextField id='none' label='CNPJ' variant='filled' name='CNPJ' margin='normal' fullWidth />
               <TextField id='none' label='Assunto' variant='filled' name='about' margin='normal' fullWidth />
               <TextField id='none' label='Descrição' variant='filled' name='descrption' margin='normal' fullWidth />
->>>>>>> db12b4bcf0c8074097015ad2f13b0900019be243
 
               <Typography variant='h6' gutterBottom component='h6' align='center' className='text4'> Insira os detalhes de sua solicitação. Um membro de nossa equipe responderá assim que for possível. </Typography>
 

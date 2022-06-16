c8cf0c35fea2e5a603390936d12cfecbdca3d82d
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-06-16 13:16:20 -0300

    @GabrielaPeres [Task:correction page providers and implementing cardsproviders ] {16/06/2022- 13:16}

---
+++ src/components/cardFounders/CardFounders.tsx
@@ -0,0 +1,54 @@
+import { CCard, CCardBody } from '@coreui/react';
+import { CCardGroup } from '@coreui/react';
+import { CCardImage } from '@coreui/react';
+import { CCardText } from '@coreui/react';
+import { CCardTitle } from '@coreui/react';
+function CardsFounders(){
+    return(
+        <>
+        <CCardGroup>
+        <CCard>
+          <CCardImage orientation="top" src="/images/react.jpg" />
+          <CCardBody>
+            <CCardTitle>Card title</CCardTitle>
+            <CCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content.
+              This content is a little bit longer.
+            </CCardText>
+            <CCardText>
+              <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+            </CCardText>
+          </CCardBody>
+        </CCard>
+        <CCard>
+          <CCardImage orientation="top" src="/images/react.jpg" />
+          <CCardBody>
+            <CCardTitle>Card title</CCardTitle>
+            <CCardText>
+              This card has supporting text below as a natural lead-in to additional content.
+            </CCardText>
+            <CCardText>
+              <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+            </CCardText>
+          </CCardBody>
+        </CCard>
+        <CCard>
+          <CCardImage orientation="top" src="/images/react.jpg" />
+          <CCardBody>
+            <CCardTitle>Card title</CCardTitle>
+            <CCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content.
+              This card has even longer content than the first to show that equal height action.
+            </CCardText>
+            <CCardText>
+              <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+            </CCardText>
+          </CCardBody>
+        </CCard>
+      </CCardGroup> 
+      </>
+     );
+   
+    
+}
+export default CardsFounders;

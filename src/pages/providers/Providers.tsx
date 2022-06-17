0cf8894d42c23a54cc42eeec1d0668656965b2ed
kaufariax <eikauanefarias@gmail.com>
2022-06-17 13:57:15 -0300

    @kaufariax [Task: Providers Page Stylization]{17/06/2022 14:00}

--- src/pages/providers/Providers.tsx
+++ src/pages/providers/Providers.tsx
@@ -1,18 +1,30 @@
 import { Button, Grid, Typography, TextField } from "@material-ui/core";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../../components/statics/navbar/Navbar';
 import { Box } from "@mui/material";
 import './Providers.css';
 import { ChangeEvent } from "react";
 import React from "react";
+import Footer from "../../components/statics/footer/Footer";
+import { toast } from "react-toastify";
 
 function Providers() {
+
+  let navigate = useNavigate();
+
+  function handleClick () {
+    toast.success('Mensagem encaminhada com Sucesso!!', {
+        theme:"colored"
+    })
+      navigate('/Home');
+ }
+ 
   return (
     <>
       <Navbar />
       <main className='provider-body'>
         <section className='provider-section'>
-          <h1 className='provider-title'>Enviar uma solicitação</h1>
+          <h1 className='provider-title'>Enviar uma <span>solicitação</span></h1>
           <form className='provider-form'>
             <input type="email" name="E-mail" className='provider-input' autoComplete="off" placeholder="E-mail" />
             <input type="cnpj" name="cnpj" className='provider-input' autoComplete="off" placeholder="CNPJ" />
@@ -23,7 +35,7 @@ function Providers() {
             Assim que for possível um Fundador(a) da Greeniverse responderá a sua solicitação.</p>
           <p className='provider-p2'>Desde já agradecemos.
           </p>
-          <button type='submit' className='btn'>Enviar </button>
+          <button type='submit' className='btn5' onClick={handleClick} >Enviar </button>
         </section>
       </main>
     </>

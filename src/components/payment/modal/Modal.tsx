6ae3a0769f28216ca95ece5c4b397a216d1934dc
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-06-20 11:33:52 -0300

    @MuriloAGama [Task: Implemeting and corrections in modal, and pages] {20/06/2022 11:33}

--- src/components/payment/modal/Modal.tsx
+++ src/components/payment/modal/Modal.tsx
@@ -19,8 +19,7 @@ function BasicModal() {
 
   function handleClick () {
     toast.success('Doação realizada com sucesso, a mesma será adicionada ao fim da compra, muito obrigada!!', {
-        autoClose: 5000,
-        
+        autoClose: 3000,
         progress: undefined,
         hideProgressBar: true
         
@@ -45,7 +44,9 @@ function BasicModal() {
 
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <p>Doe para quem precisa e receba nossa moeda para trocar por descontos e cupons!</p>
+            <p>Doe para quem precisa e receba nossa moeda para trocar por descontos e cupons!
+              Doando recebe Gcoins para trocar por descontos em próximas compras e 
+            </p>
 
             <div className='bt-donation'>
               
@@ -53,6 +54,16 @@ function BasicModal() {
             <button type="submit" value="doa10" className="btn-doa20"  >R$ 20<a className='iconDoa20'><FavoriteBorderIcon style={{ fontSize: 30, color: "red" }} onClick={handleClick} /></a></button>
             <button type="submit" value="doa10" className="btn-doa30"  >R$ 30<a className='iconDoa30'><FavoriteBorderIcon style={{ fontSize: 30, color: "red" }} onClick={handleClick} /></a></button>
             </div>
+            
+            <div className='textGcoins'>
+              <ul className='textGcoins1'>
+                <li> +100 Gcoins</li>
+                <li> +300 Gcoins</li>
+                <li> +500 Gcoins</li>
+              </ul>
+              
+            </div>
+            
 
           </Typography>
         </Box>

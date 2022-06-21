be79e0deba133c639b246714890f6ca937f9d430
kaufariax <eikauanefarias@gmail.com>
2022-06-21 16:21:38 -0300

    @kaufariax [Task: Founders Card Stylization]{21/06/2022 16:22}

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -2,17 +2,16 @@ import TextAboutUs from '../../components/textAboutUs/TextAboutUs';
 import React from 'react';
 import './AboutUs.css';
 import Navbar from '../../components/statics/navbar/Navbar';
-import CardsFounders from '../../components/cardFounders/CardFounders';
+import CardsFlip from '../../components/cardsFoundersFlip/CardsFoundersFlip';
 
 function AboutUs() {
     return (
         <>
             <main id='about_main'>
-
                 <Navbar />
                 <TextAboutUs />
-                <section className='cardsFounders'>
-                <CardsFounders />
+                <section >
+                    <CardsFlip />
                 </section>
             </main>
 
